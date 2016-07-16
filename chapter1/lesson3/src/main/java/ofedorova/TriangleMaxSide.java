e757008b10642bdddb826dc1506cd1c06b1e2d4a
OlgaFedorova <olga.fedorova.poluhina@gmail.com>
2016-07-16 14:56:28 +0400

    chapter1/lesson3/src/main/java/ofedorova/TriangleMaxSide.java

---
+++ chapter1/lesson3/src/main/java/ofedorova/TriangleMaxSide.java
@@ -0,0 +1,25 @@
+package ofedorova;
+
+/**
+ *Class класс для определения максимальной стороны треугольника
+ *@author Olga Fedorova
+ *@since 16.07.2016
+ *@version 1
+ */
+ public class TriangleMaxSide {
+	 
+	 public double max(Triangle triangle){
+		double sideAB = triangle.getA().distanceTo(triangle.getB());
+		double sideAC = triangle.getA().distanceTo(triangle.getC());
+		double sideBC = triangle.getB().distanceTo(triangle.getC());
+		
+		double max = sideAB;
+		if(sideAC > sideAB && sideAC > sideBC)
+			max = sideAC;
+		else if(sideBC > sideAC && sideBC > sideAB)
+			max = sideBC;
+		
+		return max;
+	 }
+ 
+ }
