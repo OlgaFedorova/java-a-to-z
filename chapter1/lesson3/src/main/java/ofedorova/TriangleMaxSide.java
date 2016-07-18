e2323c21af8022ae271cfb9ca41c8ead7e066f37
OlgaFedorova <olga.fedorova.poluhina@gmail.com>
2016-07-18 15:06:53 +0400

    change metod max

--- chapter1/lesson3/src/main/java/ofedorova/TriangleMaxSide.java
+++ chapter1/lesson3/src/main/java/ofedorova/TriangleMaxSide.java
@@ -10,19 +10,27 @@ package ofedorova;
 	 
 	 /**
 	  *Вычисляет максимальную сторону треугольника
+	  *@param triangle геометрическая фигура
+	  *@return максимальная сторона
 	  */
-	 public double max(Triangle triangle){
+	 public double maxSide(Triangle triangle){
 		double sideAB = triangle.getA().distanceTo(triangle.getB());
 		double sideAC = triangle.getA().distanceTo(triangle.getC());
 		double sideBC = triangle.getB().distanceTo(triangle.getC());
 		
-		double max = sideAB;
-		if(sideAC > sideAB && sideAC > sideBC)
-			max = sideAC;
-		else if(sideBC > sideAC && sideBC > sideAB)
-			max = sideBC;
-		
-		return max;
+		return max(sideAB, sideAC, sideBC);
+	 }
+	 
+	 /**
+	  *Вычисляет максимальное число
+	  *@param numbers массив double
+	  *@return максимальное число
+	  */
+	 public double max(double ... numbers){
+		 double max = numbers[0];
+		 for(int i = 1; i < numbers.length;i++)
+			 max = Math.max(max, numbers[i]); 
+		 return max;
 	 }
  
  }
