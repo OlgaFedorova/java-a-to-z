10f7185b9652eb47d9e0c237d462bff75888bf12
OlgaFedorova <olga.fedorova.poluhina@gmail.com>
2016-07-18 17:15:08 +0400

    modified lesson4/src/test/java/ofedorova/SquareTest.java

--- chapter1/lesson4/src/test/java/ofedorova/SquareTest.java
+++ chapter1/lesson4/src/test/java/ofedorova/SquareTest.java
@@ -16,10 +16,10 @@ public class SquareTest {
 	@Test
 	public void whenCalculate(){
 		final Square square = new Square(3,1,4);
-        float checked = 18f;
+        	float checked = 18f;
 
-        final float result = square.calculate(2);;
-        assertThat(result, is(checked));
+        	final float result = square.calculate(2);;
+        	assertThat(result, is(checked));
 	}
 	
 	
