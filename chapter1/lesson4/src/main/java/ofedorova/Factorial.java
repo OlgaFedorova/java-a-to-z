01406cb1ad011dec8e342c915ff3a31198f309b5
OlgaFedorova <olga.fedorova.poluhina@gmail.com>
2016-07-18 17:04:39 +0400

    add lesson4/src/main/java/ofedorova/Factorial.java

---
+++ chapter1/lesson4/src/main/java/ofedorova/Factorial.java
@@ -0,0 +1,39 @@
+package ofedorova;
+
+/**
+ *Class класс для вычисления факториала
+ *@author Olga Fedorova
+ *@since 18.07.2016
+ *@version 1
+ */
+ public class Factorial{
+	 /**
+	  *The number for factorial
+	  */
+	 private int number;
+	 
+	 public Factorial(int number){
+		 this.number = number;
+	 }
+	 
+	 public int getNumber(){
+		 return this.number;
+	 }
+	 
+	 public void setNumber(int number){
+		 this.number = number;
+	 }
+	 
+	 /**
+	  *Compute factorial for number
+	  *@return value of factorial
+	  */
+	 public int compute(){
+		 int result = 1; 
+		 for (int i = 2; i <= this.getNumber(); i++){
+			result *= i;
+		 }
+		 return result;		 
+	 }
+
+ }
