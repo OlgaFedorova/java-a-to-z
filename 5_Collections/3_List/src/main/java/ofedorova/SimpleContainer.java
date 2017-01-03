447b03c4267cfe91515e6fa9c1247704cd3b8ebd
OlgaFedorova <olga.fedorova.poluhina@gmail.com>
2017-01-03 20:16:21 +0400

    create dynamic list

--- 5_Collections/3_List/src/main/java/ofedorova/SimpleContainer.java
+++ 5_Collections/3_List/src/main/java/ofedorova/SimpleContainer.java
@@ -8,4 +8,14 @@ public interface SimpleContainer<T> extends Iterable<T> {
     void add(T element);
 
     T get(int index);
+
+    T set(int index, T element);
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean contains(T element);
+
+    Object[] toArray();
 }
